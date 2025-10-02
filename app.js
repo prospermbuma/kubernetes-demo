6a0b5453b9aac45c87cd76731c9e9da487f52b57
prospermbuma <prospermbuma@gmail.com>
2025-10-03 00:26:35 +0300

    app updates

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { time } from 'console';
 import express from 'express';
 
 const app = express();
@@ -6,7 +7,24 @@ const PORT = process.env.PORT || 3000;
 
 // Default route
 app.get('/', (req, res) => {
-    res.json({ success: true, message: 'This is Kubernetes Demo Application.' });
+    res.json(
+        {
+            message: 'This is Kubernetes Demo Application.',
+            service: 'kubernetes-demo-app',
+            pod: process.env.POD_NAME || 'unknown',
+            time: new Date().toISOString(),
+        }
+    );
+});
+
+// Readiness check route
+app.get('/ready', (req, res) => {
+    res.status(200).json({ status: 'Ready' });
+});
+
+// Health check route
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'Ok' });
 });
 
 // Start the server
